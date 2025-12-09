5daee843a1af62b4343461a2c60b24890a9084c4
Christian Ortner <christian.ortner@tugraz.at>
2025-12-09 16:35:02 +0100

    fix select query for postgres

--- classes/task/check_released_videos_task.php
+++ classes/task/check_released_videos_task.php
@@ -59,7 +59,7 @@ class check_released_videos_task extends scheduled_task {
     public function execute() {
         global $DB;
         // Step 1: Get all released videos.
-        $sql = 'SELECT DISTINCT(identifier), courseid, title FROM {local_oer_snapshot} ' .
+        $sql = 'SELECT DISTINCT(identifier), courseid, title, releasenumber FROM {local_oer_snapshot} ' .
             'WHERE identifier LIKE ? ORDER BY releasenumber DESC';
         $released = $DB->get_records_sql($sql, ['oer:opencast@%']);
         cli_writeln(count($released) . ' release snapshots for opencast videos will be checked.');
