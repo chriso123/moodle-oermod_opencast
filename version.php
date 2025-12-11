b9e29b4082a524e92d780e9683629fbd3b3fee09
Christian Ortner <christian.ortner@tugraz.at>
2025-12-11 14:30:20 +0100

    increase version

--- version.php
+++ version.php
@@ -28,7 +28,7 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->version = 2025112500;
 $plugin->requires = 2024100707;
 $plugin->component = 'oermod_opencast';
-$plugin->release = 'v1.0.3';
+$plugin->release = 'v1.0.4';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->dependencies = [
         'local_oer' => 2023111701, // Older versions do not have mod subplugin support.
